e186aa708a4e8305fa750140f9843c510db87d0c
sharipovvzoirbek <zabyjorda@gmail.com>
2023-10-28 14:00:30 +0500

    viloyatlar

---
+++ viloyat.py
@@ -0,0 +1,3 @@
+xududlar = ["Qoraqalpoqiston","tashkentshaxar","Tashken","Samarqand","Buxara","Andijon","Fergana","Xorazim","Namangan","Qashqadaryo","Jizzax","Sirdaryo","Navoiy","Surxandaryo"]
+a = xududlar[14-1]
+print(a)
